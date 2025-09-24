c6d86ae47360989f66474a35de6e2da305442ec8
Pedro Simoes <176055646+404PeterSimoes@users.noreply.github.com>
2025-09-24 17:34:57 +0100

    Ion-color-primary changed to black

--- src/pages/Mapa.tsx
+++ src/pages/Mapa.tsx
@@ -1,7 +1,15 @@
+import { IonContent, IonHeader } from '@ionic/react';
+
 const Mapa = () => {
-    return(
+    return (
+        <>
         <h1>Mapa</h1>
-    )
-}
+        {/*
+            <IonHeader>aaa</IonHeader>
+            <IonContent>asdasd</IonContent>
+            */}
+        </>
+    );
+};
 
 export default Mapa;
