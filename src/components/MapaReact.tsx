b566ccc0224dfcc8362345045965df42bce24157
Pedro Simões <176055646+404PeterSimoes@users.noreply.github.com>
2025-10-09 17:24:41 +0100

    first marker working

--- src/components/MapaReact.tsx
+++ src/components/MapaReact.tsx
@@ -1,22 +1,34 @@
 import { IonPage, IonContent } from '@ionic/react';
-import Map from 'react-map-gl/mapbox';
+import Map, { Marker } from 'react-map-gl/mapbox';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import markerEcoponto from "../assets/marker_ecoponto.png"
+import "./MapaReact.css"
 
 const Mapa: React.FC = () => {
     return (
         <IonPage>
-            <IonContent fullscreen>
+            <IonContent>
                 <Map
                     mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
                     initialViewState={{
-                        longitude: -122.4,
-                        latitude: 37.8,
-                        zoom: 14,
+                        longitude: -8.525892398242028,
+                        latitude: 38.96373575159655,
+                        zoom: 14.12,
                     }}
                     style={{ width: '100%', height: '100%' }}
+                    //mapStyle="mapbox://styles/mapbox/streets-v9"
                     mapStyle="mapbox://styles/mapbox/standard-satellite"
                     attributionControl={false}
-                />
+                >
+                    <Marker
+                        longitude={-8.51704650887078}
+                        latitude={38.9850531036262}
+                        anchor="bottom"
+                    >
+                        <img className="imgEcoponto" src={markerEcoponto} />
+                    </Marker>
+                </Map>
+                ;
             </IonContent>
         </IonPage>
     );
