115cb407a5ebe7a8dd27de505a10fd229c747bd7
Pedro Simões <176055646+404PeterSimoes@users.noreply.github.com>
2025-10-13 15:36:21 +0100

    markers being loaded by an array - test

--- src/components/MapaReact.tsx
+++ src/components/MapaReact.tsx
@@ -1,18 +1,24 @@
 import { IonPage, IonContent } from '@ionic/react';
 import Map, { Marker } from 'react-map-gl/mapbox';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import markerEcoponto from "../assets/marker_ecoponto.png"
-import "./MapaReact.css"
+import markerEcoponto from '../assets/marker_ecoponto.png';
+import './MapaReact.css';
 
 const Mapa: React.FC = () => {
+    const ecopontos = [
+        { id: 1, latitude: 38.9850531036262, longitude: -8.51704650887078 },
+        { id: 2, latitude: 38.9590289941464, longitude: -8.52303300697236 },
+        { id: 3, latitude: 38.9620051339888, longitude: -8.52243435377444 },
+    ];
+
     return (
         <IonPage>
             <IonContent>
                 <Map
                     mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
                     initialViewState={{
-                        longitude: -8.525892398242028,
                         latitude: 38.96373575159655,
+                        longitude: -8.525892398242028,
                         zoom: 14.12,
                     }}
                     style={{ width: '100%', height: '100%' }}
@@ -20,15 +26,12 @@ const Mapa: React.FC = () => {
                     mapStyle="mapbox://styles/mapbox/standard-satellite"
                     attributionControl={false}
                 >
-                    <Marker
-                        longitude={-8.51704650887078}
-                        latitude={38.9850531036262}
-                        anchor="bottom"
-                    >
-                        <img className="imgEcoponto" src={markerEcoponto} />
-                    </Marker>
+                    {ecopontos.map((eco) => (
+                        <Marker latitude={eco.latitude} longitude={eco.longitude} anchor="bottom">
+                            <img className="imgEcoponto" src={markerEcoponto} />
+                        </Marker>
+                    ))}
                 </Map>
-                ;
             </IonContent>
         </IonPage>
     );
