e7e22ea75010d8141455b6c62a9bec6a4112e8bb
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2025-11-29 19:14:53 +0000

    first version 'modalResiduos' buttons

--- src/components/Modal/modalResiduos.tsx
+++ src/components/Modal/modalResiduos.tsx
@@ -1,14 +1,55 @@
-import { IonContent, IonHeader, IonToolbar, IonPage } from '@ionic/react';
+import {
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    IonPage,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonToggle,
+    IonGrid,
+    IonRow,
+    IonCol,
+} from '@ionic/react';
+import './modalResiduos.css'
 
 const ModalPageResiduos: React.FC = () => {
     return (
         <IonPage>
             <IonHeader>
                 <IonToolbar>
-                    <h3 style={{ textAlign: 'center', marginTop:'none' }}>Resíduos</h3>
+                    <h3 style={{ textAlign: 'center', marginTop: 'none' }}>Resíduos</h3>
                 </IonToolbar>
             </IonHeader>
-            <IonContent></IonContent>
+            <IonContent>
+                <IonGrid>
+                    <IonRow>
+                        <IonCol><div>Tudo</div></IonCol>
+                        <IonCol><div>Limpar</div></IonCol>
+                    </IonRow>
+                </IonGrid>
+                <IonGrid>
+                    <IonRow>
+                        <IonCol>
+                            <div className='papel'>Papel</div>
+                        </IonCol>
+                        <IonCol>
+                            <div className='plastico'>Plástico</div>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow>
+                        <IonCol>
+                            <div className='vidro'>Vidro</div>
+                        </IonCol>
+                        <IonCol>
+                            <div className='oleao'>Oleão</div>
+                        </IonCol>
+                        <IonCol>
+                            <div className='pilhao'>Pilhão</div>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+            </IonContent>
         </IonPage>
     );
 };
