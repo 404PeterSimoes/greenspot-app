e9e9f0f43c1d9a681dc8440d606633a8d2e54651
Pedro Simões <176055646+404PeterSimoes@users.noreply.github.com>
2026-01-13 15:38:30 +0000

    no changes

--- src/App.tsx
+++ src/App.tsx
@@ -188,7 +188,6 @@ const AppContent: React.FC = () => {
           backdropDismiss={false}
           expandToScroll={false}
           handle={false}
-          handleBehavior='cycle'
         >
           <ModalPageResiduos />
         </IonModal>
