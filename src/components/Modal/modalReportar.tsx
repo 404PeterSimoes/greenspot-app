c56d1dbe6f7af13c2273ad4b81899a4a6ae4aa59
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-02-28 14:45:33 +0000

    feat: new 'problem' state for modalReport

--- src/components/Modal/modalReportar.tsx
+++ src/components/Modal/modalReportar.tsx
@@ -21,7 +21,93 @@ interface Props {
   setDesignSelected: (value: string) => void;
 }
 
+interface ProblemaProps {
+  Local: string | undefined | null;
+  Coordenadas: string | undefined | null;
+  Concelho: string | undefined | null;
+  Freguesia: string | undefined | null;
+  Problema: string | undefined | null;
+}
+
 const ModalPageReportar: React.FC<Props> = ({ setModalReportar, setDesignSelected }) => {
+  const { selectedEcoponto } = useContext(EcopontosContext);
+
+  const [problema, setProblema] = useState<ProblemaProps>({
+    Local: '',
+    Coordenadas: '',
+    Concelho: '',
+    Freguesia: '',
+    Problema: '',
+  });
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons
+            onClick={() => {
+              setModalReportar(false);
+              setDesignSelected('mapa');
+            }}
+            slot="start"
+          >
+            <IonBackButton defaultHref="#" />
+          </IonButtons>
+          <IonTitle style={{ color: '#272727' }}>Reportar problema</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <div className="divContentReportar" style={{ marginLeft: '16px', marginRight: '16px', marginTop: '24px' }}>
+          <IonInput
+            label="Local"
+            labelPlacement="floating"
+            value={problema.Local || `${selectedEcoponto?.Morada} (${selectedEcoponto?.Tipologia})`}
+            fill="outline"
+            onIonChange={(e) => {
+              setProblema({ ...problema, Local: e.detail.value });
+              console.log(problema);
+            }} // quase a funcionar
+          />
+          <br />
+          <IonInput
+            label="Coordenadas"
+            labelPlacement="floating"
+            value={`${selectedEcoponto?.Latitude}   ${selectedEcoponto?.Longitude}`}
+            fill="outline"
+            disabled={true}
+          />
+          <br />
+          <IonInput
+            label="Concelho"
+            labelPlacement="floating"
+            value={selectedEcoponto?.Concelho}
+            fill="outline"
+            disabled={true}
+          />
+          <br />
+          <IonInput
+            label="Freguesia"
+            labelPlacement="floating"
+            value={selectedEcoponto?.Freguesia}
+            fill="outline"
+            disabled={true}
+          />
+          <br />
+          <IonSelect label="Problema" labelPlacement="floating" interface="popover" fill="outline">
+            <IonSelectOption value="danificado">Danificado</IonSelectOption>
+            <IonSelectOption value="deslocado">Deslocado</IonSelectOption>
+            <IonSelectOption value="inexistente">Inexistente</IonSelectOption>
+            <IonSelectOption value="outro">Outro</IonSelectOption>
+          </IonSelect>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default ModalPageReportar;
+
+/*const ModalPageReportar: React.FC<Props> = ({ setModalReportar, setDesignSelected }) => {
   const { selectedEcoponto, arrayEcopontos } = useContext(EcopontosContext);
   const [concelhos, setConcelhos] = useState<string[]>([]);
   const [freguesias, setFreguesias] = useState<string[]>([]);
@@ -109,3 +195,4 @@ const ModalPageReportar: React.FC<Props> = ({ setModalReportar, setDesignSelecte
 };
 
 export default ModalPageReportar;
+*/
